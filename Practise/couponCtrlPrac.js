52e405e7e8bd312ab50397a5233966ed347f84c5
Karthik Ch <saipavan39dh@gmail.com>
2023-12-02 22:23:58 +0530

    Some Models Are Modified

--- Practise/couponCtrlPrac.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import coupon from '../models/couponModel.js'
-import asyncHandler from 'express-async-handler'
-export const couponCreate =asyncHandler(async(req,res)=>{
-    const couponCreation = req.body;
-
-})
--- index.js
+++ index.js
@@ -22,8 +22,8 @@ import cookieParser from 'cookie-parser'
 const app = express()
 const PORT = process.env.PORT || 8080
 
-const __filename = fileURLToPath(import.meta.url);  
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);  
+// const __dirname = path.dirname(__filename);
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
