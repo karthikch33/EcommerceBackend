15a70f7fba961f8c76875659558eaecc3ce3fc15
Karthik Ch <saipavan39dh@gmail.com>
2024-06-01 16:18:11 +0530

    session time decreased

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 
 const generateToken = (id) =>{
-    return jwt.sign({id:id},process.env.JWT_SECRET,{expiresIn:'3d'})
+    return jwt.sign({ id: id }, process.env.JWT_SECRET, { expiresIn: '10s' });
 }
 
 export default generateToken
